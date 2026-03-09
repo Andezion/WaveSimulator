468d9d1f7fc05acda04419c6f0a1ac048e8276e0
Andezion <245122@edu.p.lodz.pl>
2026-03-09 20:44:39 +0100

    add: add new wave

--- waves_math/unit_impulse.h
+++ waves_math/unit_impulse.h
@@ -4,13 +4,18 @@
 class UnitImpulse : public Signal {
 public:
     UnitImpulse() = default;
-    // For discrete signal, generate() iterates n from sampleStart
-    // computeValue receives t = n / samplingFreq
-    double computeValue(double t) const override;
-    std::string getName() const override { return "S10: Unit Impulse"; }
-    bool isPeriodic() const override { return false; }
-    bool isDiscrete() const override { return true; }
 
-    // Override generate to use integer sample indices
+    double computeValue(double t) const override;
+
+    std::string getName() const override { 
+        return "S10: Unit Impulse"; 
+    }
+    bool isPeriodic() const override { 
+        return false; 
+    }
+    bool isDiscrete() const override { 
+        return true; 
+    }
+
     void generateDiscrete();
 };
