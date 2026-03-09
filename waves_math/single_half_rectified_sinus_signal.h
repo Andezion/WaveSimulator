c56213487fb519ae6b7efec736a3152be9279eb5
Andezion <245122@edu.p.lodz.pl>
2026-03-09 17:52:20 +0100

    add: add new wave

--- waves_math/single_half_rectified_sinus_signal.h
+++ waves_math/single_half_rectified_sinus_signal.h
@@ -5,7 +5,14 @@ class SingleHalfRectifiedSinusSignal : public Signal {
 public:
     SingleHalfRectifiedSinusSignal() = default;
     double computeValue(double t) const override;
-    std::string getName() const override { return "S4: Half-Wave Rect. Sine"; }
-    bool isPeriodic() const override { return true; }
-    bool isDiscrete() const override { return false; }
+
+    std::string getName() const override { 
+        return "S4: Half-Wave Rect. Sine"; 
+    }
+    bool isPeriodic() const override { 
+        return true; 
+    }
+    bool isDiscrete() const override { 
+        return false; 
+    }
 };
