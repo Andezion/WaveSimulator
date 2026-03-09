5d737b77872b698331f9e1ce708418665bed10a1
Andezion <245122@edu.p.lodz.pl>
2026-03-09 20:42:58 +0100

    add: add new wave

--- waves_math/square_wave_signal.cpp
+++ waves_math/square_wave_signal.cpp
@@ -4,14 +4,25 @@
 double SquareWaveSignal::computeValue(double t) const {
     double t1 = params.startTime;
     double t2 = t1 + params.duration;
-    if (t < t1 || t > t2) return 0.0;
+
+    if (t < t1 || t > t2) {
+        return 0.0;
+    }
+
     double T  = params.period;
     double kw = params.dutyCycle;
-    if (T <= 0.0) return 0.0;
-    // Phase within current period
+
+    if (T <= 0.0) {
+        return 0.0;
+    }
     double phase = std::fmod(t - t1, T);
-    if (phase < 0.0) phase += T;
-    // A in [0, kw*T), 0 in [kw*T, T)
-    if (phase < kw * T) return params.amplitude;
+
+    if (phase < 0.0) {
+        phase += T;
+    }
+
+    if (phase < kw * T) {
+        return params.amplitude;
+    }
     return 0.0;
 }
