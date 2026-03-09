7e67dc2ddcdeb49f816d46722279bebcecfd6a80
Andezion <245122@edu.p.lodz.pl>
2026-03-09 17:52:58 +0100

    add: add new wave

--- waves_math/single_half_rectified_sinus_signal.cpp
+++ waves_math/single_half_rectified_sinus_signal.cpp
@@ -4,10 +4,16 @@
 double SingleHalfRectifiedSinusSignal::computeValue(double t) const {
     double t1 = params.startTime;
     double t2 = t1 + params.duration;
-    if (t < t1 || t > t2) return 0.0;
+
+    if (t < t1 || t > t2) {
+        return 0.0;
+    }
     double T = params.period;
-    if (T <= 0.0) return 0.0;
+
+    if (T <= 0.0) {return 0.0;
+        return 0.0;
+    }
+
     double sinVal = std::sin(2.0 * M_PI / T * (t - t1));
-    // x(t) = A/2 * (sin(...) + |sin(...)|)
     return (params.amplitude / 2.0) * (sinVal + std::abs(sinVal));
 }
