9b53867fe7a1211ca72728862a09d891c97a32c4
Andezion <245122@edu.p.lodz.pl>
2026-03-09 17:53:29 +0100

    add: add new wave

--- waves_math/sinus_signal.cpp
+++ waves_math/sinus_signal.cpp
@@ -4,8 +4,14 @@
 double SinusSignal::computeValue(double t) const {
     double t1 = params.startTime;
     double t2 = t1 + params.duration;
-    if (t < t1 || t > t2) return 0.0;
+
+    if (t < t1 || t > t2) {
+        return 0.0;
+    }
     double T = params.period;
-    if (T <= 0.0) return 0.0;
+
+    if (T <= 0.0) { 
+        return 0.0;
+    }
     return params.amplitude * std::sin(2.0 * M_PI / T * (t - t1));
 }
