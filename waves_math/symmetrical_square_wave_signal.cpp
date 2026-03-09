cfb76ed84e19db9030dc12669870c2174b76b275
Andezion <245122@edu.p.lodz.pl>
2026-03-09 20:49:11 +0100

    add: add new wave

--- waves_math/symmetrical_square_wave_signal.cpp
+++ waves_math/symmetrical_square_wave_signal.cpp
@@ -4,13 +4,24 @@
 double SymmetricalSquareWaveSignal::computeValue(double t) const {
     double t1 = params.startTime;
     double t2 = t1 + params.duration;
-    if (t < t1 || t > t2) return 0.0;
+
+    if (t < t1 || t > t2) {
+        return 0.0;
+    }
     double T  = params.period;
     double kw = params.dutyCycle;
-    if (T <= 0.0) return 0.0;
+
+    if (T <= 0.0) {
+        return 0.0;
+    }
     double phase = std::fmod(t - t1, T);
-    if (phase < 0.0) phase += T;
-    // A in [0, kw*T), -A in [kw*T, T)
-    if (phase < kw * T) return params.amplitude;
+
+    if (phase < 0.0) {
+        phase += T;
+    }
+
+    if (phase < kw * T) {
+        return params.amplitude;
+    }
     return -params.amplitude;
 }
