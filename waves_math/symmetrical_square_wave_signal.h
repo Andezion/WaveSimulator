d3cfacdb33935b245161652b5d239757144bec4f
Andezion <245122@edu.p.lodz.pl>
2026-03-09 20:48:29 +0100

    add: add new wave

--- waves_math/symmetrical_square_wave_signal.h
+++ waves_math/symmetrical_square_wave_signal.h
@@ -5,7 +5,14 @@ class SymmetricalSquareWaveSignal : public Signal {
 public:
     SymmetricalSquareWaveSignal() = default;
     double computeValue(double t) const override;
-    std::string getName() const override { return "S7: Sym. Square Wave"; }
-    bool isPeriodic() const override { return true; }
-    bool isDiscrete() const override { return false; }
+
+    std::string getName() const override { 
+        return "S7: Sym. Square Wave"; 
+    }
+    bool isPeriodic() const override { 
+        return true; 
+    }
+    bool isDiscrete() const override { 
+        return false; 
+    }
 };
