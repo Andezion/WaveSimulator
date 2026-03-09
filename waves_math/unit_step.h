a66a8a36c4252bb6be1f0bac1221626be6351a53
Andezion <245122@edu.p.lodz.pl>
2026-03-09 20:43:15 +0100

    add: add new wave

---
+++ waves_math/unit_step.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "signal.h"
+
+class UnitStep : public Signal {
+public:
+    UnitStep() = default;
+    double computeValue(double t) const override;
+    std::string getName() const override { return "S9: Unit Step"; }
+    bool isPeriodic() const override { return false; }
+    bool isDiscrete() const override { return false; }
+};
