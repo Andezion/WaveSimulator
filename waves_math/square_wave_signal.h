f69c0340adafb6d872dfaf751b58a1e2e842b3f1
Andezion <245122@edu.p.lodz.pl>
2026-03-09 20:42:01 +0100

    add: add new wave

--- waves_math/square_wave_signal.h
+++ waves_math/square_wave_signal.h
@@ -5,7 +5,13 @@ class SquareWaveSignal : public Signal {
 public:
     SquareWaveSignal() = default;
     double computeValue(double t) const override;
-    std::string getName() const override { return "S6: Square Wave"; }
-    bool isPeriodic() const override { return true; }
-    bool isDiscrete() const override { return false; }
+    std::string getName() const override { 
+        return "S6: Square Wave"; 
+    }
+    bool isPeriodic() const override { 
+        return true; 
+    }
+    bool isDiscrete() const override { 
+        return false; 
+    }
 };
