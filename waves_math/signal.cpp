5526b8404f35c939cc53e1cd57efda4b9972c04a
Andezion <245122@edu.p.lodz.pl>
2026-03-10 19:10:12 +0100

    add: added new signal

--- waves_math/signal.cpp
+++ waves_math/signal.cpp
@@ -6,7 +6,9 @@ void Signal::generate() {
 
     if (isDiscrete()) {
         int numSamples = static_cast<int>(params.duration * params.samplingFreq);
-        if (numSamples <= 0) numSamples = 1;
+        if (numSamples <= 0) { 
+            numSamples = 1;
+        }
         for (int n = params.sampleStart; n < params.sampleStart + numSamples; ++n) {
             double t = static_cast<double>(n) / params.samplingFreq;
             times.push_back(t);
