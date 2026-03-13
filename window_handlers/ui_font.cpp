c917a999eb5cc827401e07ecbd0312b37bb02eba
Andezion <245122@edu.p.lodz.pl>
2026-03-13 17:33:56 +0100

    fix: separated files

---
+++ window_handlers/ui_font.cpp
@@ -0,0 +1,31 @@
+#include "ui_font.h"
+
+static Font g_font     = {};
+static bool g_fontLoaded = false;
+
+void initDrawFont() {
+    g_font = LoadFontEx("/usr/share/fonts/liberation-sans-fonts/LiberationSans-Regular.ttf",
+                        20, nullptr, 0);
+    g_fontLoaded = (g_font.texture.id > 0);
+}
+
+void cleanupDrawFont() {
+    if (g_fontLoaded) {
+        UnloadFont(g_font);
+    }
+}
+
+void drawText(const char* text, int x, int y, int size, Color color) {
+    if (g_fontLoaded) {
+        DrawTextEx(g_font, text, {(float)x, (float)y}, (float)size, 1.0f, color);
+    } else {
+        DrawText(text, x, y, size, color);
+    }
+}
+
+int measureText(const char* text, int size) {
+    if (g_fontLoaded) {
+        return static_cast<int>(MeasureTextEx(g_font, text, static_cast<float>(size), 1.0f).x);
+    }
+    return MeasureText(text, size);
+}
