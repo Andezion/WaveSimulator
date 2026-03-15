35376e63639ca9ce2fda4c8768589997db34b523
Andezion <245122@edu.p.lodz.pl>
2026-03-15 22:15:45 +0100

    fix: cleaning up

--- window_handlers/ui_widgets.h
+++ window_handlers/ui_widgets.h
@@ -2,6 +2,9 @@
 #include <raylib.h>
 #include "../app_state.h"
 
+// Функция для отрисовки кнопок (принимает прямоугольник, текст и флаг активности для изменения цвета)
 void drawButton(Rectangle r, const char* label, bool active);
+// Функция для обработки ввода в текстовое поле (принимает структуру TextInput и обновляет её в зависимости от нажатых клавиш)
 void handleTextInputKey(TextInput& ti);
+// Функция для отрисовки текстового поля с меткой (принимает состояние приложения, индекс параметра, прямоугольник для отрисовки и текст метки)
 void drawTextInput(AppState& state, int idx, Rectangle r, const char* label);
