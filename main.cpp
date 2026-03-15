0bf2e81a86ecd18e5d2cc3322888b064b6db215c
Andezion <245122@edu.p.lodz.pl>
2026-03-15 21:34:46 +0100

    fix: cleaning up

--- main.cpp
+++ main.cpp
@@ -4,15 +4,20 @@
 #include <cstdlib>
 #include <ctime>
 
+// Функции для обработки ввода и ограничения прокрутки
 void processInput(AppState& state, int plotX, int plotW);
+// Функция для ограничения прокрутки графика
 void clampPlotScroll(AppState& state, float plotDrawWidth);
 
 int main() {
+    // Инициализация генератора случайных чисел для шумов
     srand(static_cast<unsigned>(time(nullptr)));
 
     const int SCREEN_W = 1400;
     const int SCREEN_H = 800;
+    // Ширина левой панели
     const int PANEL_W = 320;
+    // Отступы для графика внутри правой области
     const int PLOT_PAD_L = 55; 
     const int PLOT_PAD_R = 10;
 
