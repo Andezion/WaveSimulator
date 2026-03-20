3fc6c54cafa824943823e0b39246b6378f253962
Andezion <245122@edu.p.lodz.pl>
2026-03-20 19:19:31 +0100

    doc: added docs

--- window_handlers/file_dialog.h
+++ window_handlers/file_dialog.h
@@ -4,6 +4,8 @@
 #include <string>
 #include "../app_state.h"
 
+// Рисуем диалоговое окно для загрузки или сохранения файла
+// Принимает заголовок окна, флаг отображения и функцию-обработчик для кнопки OK, которая получает путь к файлу, выбранному пользователем
 void drawFileDialog(AppState& state, const char* title,
                     bool& showFlag,
                     std::function<void(const std::string&)> onOk);
