693d6f4862def20c62370dbf35e69b889238836d
Andezion <245122@edu.p.lodz.pl>
2026-03-20 19:25:17 +0100

    doc: added docs

--- file_handlers/file_io.h
+++ file_handlers/file_io.h
@@ -3,6 +3,7 @@
 #include <string>
 #include <memory>
 
-
+// Функции для сохранения сигнала в файл и загрузки сигнала из файла
+// Эти функции используются в диалогах сохранения и загрузки сигнала в главном окне приложения
 bool saveSignal(const Signal& sig, const std::string& path);
 std::unique_ptr<Signal> loadSignal(const std::string& path);
